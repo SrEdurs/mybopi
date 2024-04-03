04e5bc8959cdc38e30c4ce25ed9d2d33d82487e6
SrEdurs <sredurs@outlook.es>
2024-04-03 14:11:40 +0200

    first commit

---
+++ src/test/java/es/mybopi/mybopi/MybopiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mybopi.mybopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybopiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
