af2856b11179f65756c1f2d91db7874bf957b65e
SrEdurs <sredurs@outlook.es>
2024-04-04 13:19:12 +0200

    - Home inicial

--- src/main/java/es/mybopi/model/DetallePedido.java
+++ src/main/java/es/mybopi/model/DetallePedido.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -34,7 +35,7 @@ public class DetallePedido {
     @OneToOne
     private Pedido pedido;
 
-    @OneToOne
+    @ManyToOne
     private Producto producto;
 
 }
