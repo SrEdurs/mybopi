217ffd7370dc80e50653cda539b35de3d6da6398
SrEdurs <sredurs@outlook.es>
2024-04-16 19:43:14 +0200

    - Producto controller actualizado

--- src/main/java/es/mybopi/controller/ProductoController.java
+++ src/main/java/es/mybopi/controller/ProductoController.java
@@ -1,22 +1,36 @@
 package es.mybopi.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import es.mybopi.model.Producto;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
 
     @GetMapping("")
     public String detalles(){
         return "productos/detalles";
     }
 
-    //Crear productos
+    //Pantalla crear productos
     @GetMapping("/crear")
     public String crearProducto(){
         return "productos/crear";
+    }
+
+    //Guardar un nuevo producto
+    @PostMapping("/guardar")
+    public String guardar(Producto producto){
+        LOGGER.info("Guardado {}", producto);
+        return "redirect:/productos";
     }
 
 }
