ea6f36122ffdb208178ab182b5cfa4ee0214edca
SrEdurs <sredurs@outlook.es>
2024-04-23 11:14:31 +0200

    - Pantalla de cada producto inicial

--- src/main/java/es/mybopi/controller/HomeController.java
+++ src/main/java/es/mybopi/controller/HomeController.java
@@ -1,11 +1,13 @@
 package es.mybopi.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.mybopi.model.Producto;
@@ -25,4 +27,12 @@ public class HomeController {
         return "usuarios/index";
     }
 
+    @GetMapping("producto/{id}")
+    public String producto(@PathVariable Integer id, Model model) {
+        Optional<Producto> optionalProducto = productoService.findById(id);
+        if (optionalProducto.isPresent()) {
+            model.addAttribute("producto", optionalProducto.get());
+        }
+        return "productos/producto";
+    }
 }
