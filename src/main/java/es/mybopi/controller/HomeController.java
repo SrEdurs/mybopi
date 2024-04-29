28b9e99cd4ff21ad88297bd46bf63bf03ba4aafc
SrEdurs <sredurs@outlook.es>
2024-04-29 13:00:29 +0200

    - Guardar pedidos

--- src/main/java/es/mybopi/controller/HomeController.java
+++ src/main/java/es/mybopi/controller/HomeController.java
@@ -1,9 +1,9 @@
 package es.mybopi.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,15 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import es.mybopi.model.DetallePedido;
 import es.mybopi.model.Pedido;
 import es.mybopi.model.Producto;
 import es.mybopi.model.Usuario;
 import es.mybopi.repository.ProductoRepository;
+import es.mybopi.service.DetallePedidoService;
+import es.mybopi.service.PedidoService;
 import es.mybopi.service.ProductoService;
 import es.mybopi.service.UsuarioService;
-
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -35,6 +35,12 @@ public class HomeController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private PedidoService pedidoService;
+
+    @Autowired
+    private DetallePedidoService detalleService;
 
     private List<DetallePedido> detalles = new ArrayList<DetallePedido>();
     private Pedido pedido = new Pedido();
@@ -160,6 +166,25 @@ public class HomeController {
         model.addAttribute("pedido", pedido);
         model.addAttribute("usuario", usuario);
         return "usuarios/resumencompra";
+    }
+
+    @GetMapping("/guardarPedido")
+    public String guardarPedido() {
+        Date fechaPedido = new Date();
+        pedido.setFecha(fechaPedido);
+        pedido.setNumero(pedidoService.generarNumPedido());
+        pedido.setUsuario(usuarioService.findById(1));
+        pedidoService.save(pedido);
+
+        for (DetallePedido dp : detalles) {
+            dp.setPedido(pedido);
+            detalleService.save(dp);
+        }
+
+        pedido = new Pedido();
+        detalles.clear();
+
+        return "redirect:/";
     }
     
 }
