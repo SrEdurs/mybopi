dbccb8775d5631c0b49d99fe8e0248d842a0f2d2
SrEdurs <sredurs@outlook.es>
2024-04-29 10:35:32 +0200

    - resumencompra inicial

--- src/main/java/es/mybopi/controller/HomeController.java
+++ src/main/java/es/mybopi/controller/HomeController.java
@@ -147,5 +147,10 @@ public class HomeController {
 
         return "usuarios/carrito";
     }
+
+    @GetMapping("/pedido")
+    public String order(Model model) {
+        return "usuarios/resumencompra";
+    }
     
 }
