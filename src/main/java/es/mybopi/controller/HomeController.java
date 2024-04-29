a44466dd1fd7df7c9364679b2ba04cf283ebd193
SrEdurs <sredurs@outlook.es>
2024-04-29 19:52:53 +0200

    - Guardar pedido actualizado

--- src/main/java/es/mybopi/controller/HomeController.java
+++ src/main/java/es/mybopi/controller/HomeController.java
@@ -39,12 +39,8 @@ public class HomeController {
 
     @Autowired
     private PedidoService pedidoService;
-
-    @Autowired
-    private DetallePedidoService detalleService;
-
-    private List<DetallePedido> detalles = new ArrayList<DetallePedido>();
     private Pedido pedido = new Pedido();
+    private List<Producto> productosCarro = new ArrayList<Producto>();
 
     @GetMapping("/")
     public String home(Model model) {
@@ -79,46 +75,28 @@ public class HomeController {
     @PostMapping("/carrito")
     public String addCarrito(@RequestParam("id") Integer id, Model model) {
 
-        DetallePedido detallePedido = new DetallePedido();
         Producto producto = new Producto();
         double sumaTotal = 0;
 
         Optional<Producto> optionalProducto = productoService.findById(id);
-        System.out.println("Producto añadido: " + optionalProducto.get().getNombre());
-        System.out.println("-------------------------");
-
         if (optionalProducto.isPresent()) {
             producto = optionalProducto.get();
-            detallePedido.setNombre(producto.getNombre());
-            detallePedido.setPrecio(producto.getPrecio());
-            detallePedido.setProducto(producto);
 
-            //Validar que el producto no se añado 2 veces
-            boolean encontrado = false;
-            for (DetallePedido dp : detalles) {
-                if (dp.getProducto().getId() == producto.getId()) {
-                    encontrado = true;
-                    break;
-                }
+            //Añadir el producto al pedido
+            productosCarro.add(producto);
+            pedido.setProductos(productosCarro);
+            
+            //Calcular el total del pedido
+            for (Producto p : productosCarro) {
+                sumaTotal += p.getPrecio();
             }
-            if (encontrado) {
-                System.out.println("El producto ya se encuentra en el carrito");
-                return "redirect:/carrito";
-            }
-
-            detalles.add(detallePedido);
-
-            //calcular sumaTotal
-            for (DetallePedido dp : detalles) {
-                sumaTotal += dp.getPrecio();
-            }
-
-
-            System.out.println("--------------"+sumaTotal);
+        
+            Usuario usuario = usuarioService.findById(1);
+            pedido.setUsuario(usuario);
+            pedido.setFecha(new Date());
+            pedido.setNumero(pedidoService.generarNumPedido());
             pedido.setTotal(sumaTotal);
-
-            System.out.println("--------------"+pedido.getTotal());
-            model.addAttribute("detalles", detalles);
+            pedidoService.save(pedido);
             model.addAttribute("pedido", pedido);
             
         }
@@ -130,22 +108,22 @@ public class HomeController {
     @GetMapping("/quitar/{id}")
     public String quitarProducto(@PathVariable Integer id, Model model) {
 
-        //Lista nueva de productos
-        List<DetallePedido> nuevaLista = new ArrayList<DetallePedido>();
-        for (DetallePedido dp : detalles) {
-            if (dp.getProducto().getId() != id) {
-                nuevaLista.add(dp);
+        //Quitar producto del pedido
+        List<Producto> nuevaListaPedido = new ArrayList<Producto>();
+        for (Producto p : productosCarro) {
+            if (p.getId() != id) {
+                nuevaListaPedido.add(p);
             }
         }
-        detalles = nuevaLista;
+        productosCarro = nuevaListaPedido;
+        pedido.setProductos(productosCarro);
 
-        //calcular sumaTotal
+        //Calcular el total del pedido
         double sumaTotal = 0;
-        for (DetallePedido dp : detalles) {
-            sumaTotal += dp.getPrecio();
+        for (Producto p : productosCarro) {
+            sumaTotal += p.getPrecio();
         }
         pedido.setTotal(sumaTotal);
-        model.addAttribute("detalles", detalles);
         model.addAttribute("pedido", pedido);
 
         return "redirect:/carrito";
@@ -155,7 +133,6 @@ public class HomeController {
     //Carrito
     @GetMapping("/carrito")
     public String carrito(Model model) {
-        model.addAttribute("detalles", detalles);
         model.addAttribute("pedido", pedido);
         return "usuarios/carrito";
     }
@@ -163,7 +140,6 @@ public class HomeController {
     @GetMapping("/pedido")
     public String order(Model model) {
         Usuario usuario = usuarioService.findById(1);
-        model.addAttribute("detalles", detalles);
         model.addAttribute("pedido", pedido);
         model.addAttribute("usuario", usuario);
         return "usuarios/resumencompra";
@@ -172,46 +148,41 @@ public class HomeController {
     @GetMapping("/guardarPedido")
     public String guardarPedido() {
 
-        System.out.println("--------------" + detalles.size());
+        
+        System.out.println("-------------------00000000000--------------");
         List<Producto> productos = new ArrayList<Producto>();
-        for (int i = 0; i < detalles.size(); i++) {
-            System.out.println("----------------------------------111111111111----------------");
-            productos.add(detalles.get(i).getProducto());
-            detalles.get(i).getProducto().setElPedido(pedido);
-            System.out.println("-------------------------" + detalles.get(i).getProducto().getNombre());
+
+        System.out.println("-------------------11111111--------------");
+         for (int i = 0; i < productosCarro.size(); i++) {
+            System.out.println("-------------------111111555555555--------------");
+            productos.add(productosCarro.get(i));
+            productosCarro.get(i).setElPedido(pedido);
         }
 
-        
-
-        
-
+        System.out.println("-------------------2222222222--------------");
 
         pedido.setProductos(productos);
+        System.out.println("-------------------33333333333--------------");
         pedido.setTotal(pedido.getTotal());
+        System.out.println("-------------------44444444444444--------------");
         Date fechaPedido = new Date();
+        System.out.println("-------------------5555555555555--------------");
         pedido.setFecha(fechaPedido);
+        System.out.println("-------------------666666666666666--------------");
         pedido.setNumero(pedidoService.generarNumPedido());
+        System.out.println("-------------------777777777777--------------");
         pedido.setUsuario(usuarioService.findById(1));
+        System.out.println("-------------------888888888888888--------------");
         pedidoService.save(pedido);
 
         //Guardar los productos del list
         for (Producto p : productos) {
             p.setVendido(true);
             productoService.save(p);
-        }
-
-        //Leer el list de productos
-        for (Producto p : productos) {
-            System.out.println("-------------------------" + p.getNombre());
-        }
-
-        System.out.println("--------------" + pedido.getProductos().size());
-        System.out.println("--------------" + pedido.getProductos().get(0).getNombre());
-        
+        }       
 
         pedido = new Pedido();
-        detalles.clear();
-
+        productosCarro.clear();
         return "redirect:/";
     }
 
